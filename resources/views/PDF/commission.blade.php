b1b5f0d0248be43bcdc9fa57266485ecb5a1679d
Chris Rocha <CRocha@IECom.local>
2023-06-19 10:15:46 -0400

    Committer: Chris Rocha <CRocha@IECom.local>
    On branch main
    Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   resources/views/PDF/commission.blade.php
    	new file:   resources/views/PDF/favicon.png

--- resources/views/PDF/commission.blade.php
+++ resources/views/PDF/commission.blade.php
@@ -8,11 +8,17 @@
         @vite('resources/css/app.css')
     </head>
     <body>
-        <table style="font-size: 12px" class="w-full">
-            <tr class="w-full font-medium">
-                <th class="text-left">Rocket MGA LLC</th>
+        <table style="font-size: 14px" class="w-full">
+            <tr class="w-full">
+                <th><img style="height: 48px;" src="C:\Users\CRocha\Documents\_Admin.RocketFlood\admin.rocketflood\resources\views\PDF\favicon.png" alt="Logo"></th>
                 <th class="text-center">Commission Payable Statement</th>
                 <th class="text-right">{{ $today }}</th>
+            </tr>
+
+            <tr style="padding-top: 10px; padding-bottom: 10px;" class="w-full">
+                <th style="color: rgb(244, 45, 45)" class="text-left"></th>
+                <th class="text-center">Statement Month - {{ $month }}</th>
+                <th class="text-right">All MGA Commissions</th>
             </tr>
             @foreach ($collection as $item)
                 <tr>
